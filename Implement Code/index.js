febdb936f0919518c181e43b3a0a44db9c7889a8
tbmanix <tubagusmanixh@gmail.com>
2022-03-21 10:07:02 +0700

    task4

---
+++ Implement Code/index.js
@@ -0,0 +1,29 @@
+// npm init (generate package json tapi bisa mengcustom isinya)
+// npm init -y (jika kita generate package json secara default)
+
+// npm install nodemon/npm i nodemon
+
+// npm install nodemon --save-dev/npm install nodemon -D = menjadikan nodemon didalam devdepedencies
+
+// depedencies = isiannnya itu akan diggunakan sampai tahap production/release
+// devdepedencies = isiannya  itu digunakan hanya ketika tahap development
+
+// npx nodemon index.js = menjalankan nodemon
+
+// npm insall = bila tidak ada node_modules yang penting ada package json berisi depedencies
+
+console.log("hello world");
+
+// function (exports, require, __filename, __dirname) {
+// console.log('hello world')
+// }
+
+// console.log(__filename)
+// console.log(__dirname) mencari tempat filenya
+
+// const data = require("./name")
+// console.log(data)
+
+// const {name, age} = require("./name")
+// console.log(name)
+// console.log(age)
---
+++ Implement Code/name.js
@@ -0,0 +1,6 @@
+const biodata = {
+    name: "tubagus",
+    age: 21
+}
+
+module.exports = biodata //mengexport ke index.js
