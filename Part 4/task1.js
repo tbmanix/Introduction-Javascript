7b34bfea14b1f975913ab71e828b940d09bf261f
tbmanix <tubagusmanixh@gmail.com>
2022-03-22 11:55:33 +0700

    part4

--- Part 4/task1.js
+++ Part 4/task1.js
@@ -1,17 +1,21 @@
 function cekPalindrom(text) {
   if (typeof text === "string") {
-    let textLower = text.toLowerCase().trim().split(" ").join(""); //lowercase->((hapus spasi kirikanan->memecah dengan spasi->menggabungkan tanpa spasi)menghilangkan semua spasi)
+    let textLower = text.toLowerCase().split(" ").join(""); //lowercase->((memecah dengan spasi->menggabungkan tanpa spasi)menghilangkan semua spasi)
     let textreverse = textLower.split("").reverse().join(""); //memecah perhuruf->reverse->menggabungkan kembali
 
-    if (textLower === textreverse) {
+    // if (textLower === textreverse) {
+    //   //cek kondisi
+    //   return console.log(`text ${text} is Palindrom`);
+    // } else {
+    //   return console.log(`text ${text} not Palindrom`);
+    // }
+    textLower === textreverse ?
       //cek kondisi
-      return console.log(`text ${text} is Palindrom`);
-    } else {
-      return console.log(`text ${text} not Palindrom`);
-    }
+      console.log(`text ${text} is Palindrom`) :
+      console.log(`text ${text} not Palindrom`)
   } else {
     return console.log("input harus string");
   }
 }
 
-cekPalindrom("   K as U r Ru S AK");
+cekPalindrom("   K as U r Ru S aAK");
